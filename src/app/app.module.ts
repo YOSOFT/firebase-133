07f3c124f9de840ef7893a5a9a8ddd403718f5b5
YOSOFT <julio.ramriez@yosoft.com.co>
2018-09-21 22:04:36 -0500

    firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,18 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule, AngularFireDatabase } from '@angular/fire/database';
+import { AngularFireAuthModule } from '@angular/fire/auth';
 
+export const firebaseConfig = {
+  apiKey: "AIzaSyBliAOXmsclquzEbhtP0ZdvxDRMz5L8-EQ",
+    authDomain: "ejemplo-firebase-54192.firebaseapp.com",
+    databaseURL: "https://ejemplo-firebase-54192.firebaseio.com",
+    projectId: "ejemplo-firebase-54192",
+    storageBucket: "ejemplo-firebase-54192.appspot.com",
+    messagingSenderId: "653305824534"
+};
 @NgModule({
   declarations: [
     MyApp,
@@ -14,7 +25,10 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -24,6 +38,7 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
+    AngularFireDatabase,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,52 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AngularFireAuth } from '@angular/fire/auth';
+import firebase from 'firebase/app';
+import { AngularFireDatabase } from '@angular/fire/database';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  email: string
+  items: Observable<any[]>;
 
-  constructor(public navCtrl: NavController) {
+  constructor(
+    public navCtrl: NavController,
+    private afAuth: AngularFireAuth,
+    afDB: AngularFireDatabase
+  ) {
 
+    this.items = afDB.list('tareas').valueChanges();
+
+    afAuth.auth.onAuthStateChanged( usuario => {
+      console.log(usuario);
+      this.email = '';
+      if(usuario){
+        this.email = usuario.email;
+      }
+    });
+  }
+
+  signInWithSocial(social) {
+    switch(social){
+      case "facebook":
+      this.afAuth.auth
+      .signInWithPopup(new firebase.auth.FacebookAuthProvider());
+      break;
+
+      case "google":
+      this.afAuth.auth
+      .signInWithPopup(new firebase.auth.GoogleAuthProvider());
+      break;
+    }
+
+  }
+
+  signOut() {
+    this.afAuth.auth.signOut();
   }
 
 }
